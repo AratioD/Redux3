443b380b91717587d94c889649beec45d08a8ff0
AratioD <antti.heroja@iki.fi>
2019-02-24 22:24:46 +0200

    constructing redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div>
         redux 3 testing
+        <Counter />
       </div>
     );
   }
