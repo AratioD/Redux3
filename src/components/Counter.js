bc38f209c4f13c3d9a88157559518b877c092367
AratioD <antti.heroja@iki.fi>
2019-02-28 15:16:23 +0200

    still missing a solution

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,40 +1,34 @@
 //counter components
 import React from 'react';
 import { connect } from 'react-redux';
-import { countNumber } from '../reducers/countReducer'
-import { toggleImportanceOf } from '../reducers/countReducer'
-import { createStore } from 'redux'
+// import { countNumber } from '../reducers/countReducer'
+// import { toggleImportanceOf } from '../reducers/countReducer'
+// import { createStore } from 'redux'
 
-console.log('toimiiko component counter')
-export function Counter({ value }) {
-    // const notes = store.getState().initialState
 
-    // console.log('counter store', state)
-    // console.log('counter store', value.count)
-    // console.log('store counter.js', store.getState())
-    return (
-        <div>
-            <h1>Counter</h1>
-            <p>This is a simple example of a React component.</p>
-            {/* <p>Current count: <strong>{store.count}</strong></p> */}
-            {/* <button onClick={props.increment}>Increment</button> */}
-        </div>
-    )
+class Counter extends React.Component {
+  
+    render() {
+        console.log(this.props.count)
+        return (
+            <div>
+                <h1>Counter</h1>
+                <p>This is a simple example of a React component.</p>
+                <p>Current count: <strong>{this.props.count}</strong></p>
+                {/* <button onClick={props.increment}>Increment</button> */}
+            </div >
+        )
+    }
+
 }
-console.log('mapstatetoprops', mapStateToProps)
 
-// store.subscribe(() => {
-//     const storeNow = store.getState()
-//     console.log(storeNow)
-//   })
-
-// täällä on ongelma
 const mapStateToProps = state => {
     console.log('counter state', state)
     console.log('counter state 2', state.count)
     return {
-        value: state.value
+        count: state.value
     }
-};
+}
+
 
 export default connect(mapStateToProps)(Counter);
